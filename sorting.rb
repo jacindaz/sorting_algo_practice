829ccd98f3866c355db6d147a99e4e845cfcf30b
Jacinda Zhong <jacindaz@gmail.com>
2016-11-06 13:20:02 -0800

    Bubble and insertion sort

---
+++ sorting.rb
@@ -0,0 +1,68 @@
+require 'pry'
+
+def bubble_sort(array)
+  max_array_index = array.length - 1
+
+  array.length.times do
+    array.each_with_index do |value, index|
+      item1 = array[index]
+      item2 = array[index + 1]
+      unless index == max_array_index
+        if item1 > item2
+          placeholder = item1
+          array[index] = array[index + 1]
+          array[index + 1] = placeholder
+        end
+      end
+    end
+  end
+
+  array
+end
+
+# bubble_sort([99, 34, 50, 1000000, 2, -88])
+
+def insertion_sort(array)
+  sorted_array = [array[0]]
+
+  array[1..-1].each do |number|
+    sorted_array_max_index = sorted_array.length - 1
+    sorted_array.each_with_index do |sorted_array_number, sorted_array_index|
+      puts "\n============="
+      puts "number: #{number}"
+      puts "sorted_array_number: #{sorted_array_number}\n\n"
+
+      puts "sorted_array_index: #{sorted_array_index}"
+      puts "sorted_array: #{sorted_array}\n\n"
+
+      puts "sorted_array_max_index: #{sorted_array_max_index}"
+      puts "sorted_array[sorted_array_max_index + 1]: #{sorted_array[sorted_array_max_index + 1]}\n\n"
+
+      puts "original array: #{array}"
+      puts "=============\n"
+
+      if number < sorted_array_number && sorted_array_index == 0 # is 3 < 6
+        sorted_array.unshift(number)
+        break
+      elsif number > sorted_array_number && sorted_array_index == sorted_array_max_index
+        sorted_array << number
+        break
+      elsif sorted_array_index != sorted_array_max_index
+        if number > sorted_array_number && number < sorted_array[sorted_array_index + 1]
+          sorted_array = sorted_array.insert(sorted_array_index + 1, number)
+          break
+        end
+      end
+    end
+  end
+
+  puts "\n============"
+  puts "FINAL RESULT:"
+  puts "sorted_array: #{sorted_array}"
+  puts "============\n\n"
+
+  sorted_array
+end
+insertion_sort([6, 3, 20, 12, 10, 2, 100, 99, -45, -20, 30])
+
+
