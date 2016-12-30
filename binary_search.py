e0bef01c897c1d7ce7645a8caf138c67157d3a63
Jacinda Zhong <jacindaz@gmail.com>
2016-12-30 14:06:13 -0800

    New binary search method, much clearer after whiteboarding!

--- binary_search.py
+++ binary_search.py
@@ -9,77 +9,29 @@ def binary_search_num_in_list(list, number_to_find):
     max_index = len(list) - 1
     halfway_index = int(round(max_index / 2))
 
-    # 52
-    # is list[halfway_index] less than or greater than 52?
-    # if list[halfway_index] is less than:
-    #   => 41 < 52
-    #   => look at upper half,
-    #   => calculate new halfway_index, which is
-    #      (index 12 - max_index) / 2
-    #   => repeat
-    #
-    # if list[halfway_index] is greater than:
-    #   => look at lower half,
-    #   => calculate new halfway_index, which is
-    #      (index 12 - min_index ) / 2
-    #   => repeat
-    #
-    # if match:
-    #   => return true
-    # else:
-    #   => continue looking
     number_not_found = True
 
-    # START HERE
-    # HOW TO KNOW WHEN TO STOP CHECKING FOR BINARY SEARCH ??
+    while number_not_found and halfway_index != max_index and halfway_index != min_index:
+        print("\n==============")
+        print("min_index: %s, max_index: %s" % (min_index, max_index))
+        print("halfway_index: %s, halfway index number: %s" % (halfway_index, list[halfway_index]))
+        print("==============\n")
 
-    # while number_not_found and (????):
-    while number_not_found:
-        if number_to_find < list[halfway_index]:
-            old_halfway_index = halfway_index
-            halfway_index = int(round((halfway_index - min_index) / 2))
-
-            print("\n===================")
-            print("original array: %s" % list)
-            print("number looking for: %s\n" % number_to_find)
-            print("old_halfway_index number: %s" % list[old_halfway_index])
-            print("halfway_index number: %s\n" % list[halfway_index])
-
-            print("old_halfway_index: %s, halfway_index: %s" % (old_halfway_index, halfway_index))
-            print("===================\n")
-
-        elif number_to_find > list[halfway_index]:
-            old_halfway_index = halfway_index
-            halfway_index = int(round((max_index - halfway_index) / 2))
-
-            print("\n===================")
-            print("original array: %s" % list)
-            print("number looking for: %s\n" % number_to_find)
-            print("old_halfway_index number: %s" % list[old_halfway_index])
-            print("halfway_index number: %s\n" % list[halfway_index])
-
-            print("old_halfway_index: %s, halfway_index: %s" % (old_halfway_index, halfway_index))
-            print("===================\n")
+        if number_to_find > list[halfway_index]:
+            min_index = halfway_index
+        elif number_to_find < list[halfway_index]:
+            max_index = halfway_index
         elif number_to_find == list[halfway_index]:
             number_not_found = False
-
-            print("\n===================")
-            print("original array: %s" % list)
-            print("number looking for: %s\n" % number_to_find)
-            print("halfway_index number: %s\n" % list[halfway_index])
-
-            print("halfway_index: %s" % halfway_index)
-            print("===================\n")
-
-            print("Found number, return True")
+            print("found number!")
             return True
+
+        halfway_index = int(round((max_index - min_index)/2)) + min_index
 
     print("Did not find number, return False")
     return False
 
-
-
-binary_search_num_in_list(primes, 59)
+binary_search_num_in_list(primes, 60)
 
 
 # WRITING OUT STEPS
