8abcf1cf7304e9ada89ed7ab4855f9f0f427deda
Jacinda Zhong <jacindaz@gmail.com>
2016-12-31 12:42:20 -0800

    Solution according to the format that Khan Academy wants, does not work yet if number is the first in the array

--- binary_search.js
+++ binary_search.js
@@ -1,6 +1,36 @@
+var doSearch = function(array, targetValue) {
+    var min = 0;
+    var max = array.length - 1;
+    var guess;
+    while(array[guess] != targetValue){
+        guess = Math.round((max - min)/2) + min;
+
+        if(array[guess] === targetValue){
+            console.log("Found number! number: " + array[guess] + ", index: " + guess);
+            return guess;
+        }
+        else if(targetValue < array[guess]){
+            max = guess;
+        }
+        else{
+            min = guess;
+        }
+    }
+    console.log("Poop, did not find number!");
+    return -1;
+};
+
+var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
+
+var result = doSearch(primes, 97);
+// var result = doSearchJacindasWay(primes, 73);
+// console.log("Found prime at index " + result);
+
+//Program.assertEqual(doSearch(primes, 73), 20);
+
 /* Returns either the index of the location in the array,
   or -1 if the array did not contain the targetValue */
-var doSearch = function(array, targetValue) {
+var doSearchJacindasWay = function(array, targetValue) {
     var min = 0;
     var max = array.length - 1;
     var halfway_index = Math.round(max / 2);
@@ -22,11 +52,3 @@ var doSearch = function(array, targetValue) {
 
     return -1;
 };
-
-var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
-
-var result = doSearch(primes, 73);
-// console.log("Found prime at index " + result);
-
-//Program.assertEqual(doSearch(primes, 73), 20);
-
